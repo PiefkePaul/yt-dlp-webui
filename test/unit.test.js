0a1b9e95d626309e446ae5372d6ac8bee0d9f2e1
PiefkePaul <57523679+PiefkePaul@users.noreply.github.com>
2026-06-03 10:44:02 +0200

    chore: Test-Infrastruktur und .env-Vars für SC Session-Cookie Fix

---
+++ test/unit.test.js
@@ -0,0 +1,14 @@
+process.env.SESSION_ENCRYPTION_KEY = 'd29ea4ebf361d8ff1d4b1d08eb452dc5';
+
+const { test } = require('node:test');
+const assert = require('node:assert/strict');
+const path = require('path');
+const fsp = require('fs/promises');
+const os = require('os');
+
+const {
+  encryptForClient,
+  decryptFromClient,
+  fetchScSession,
+  writeTempCookieFile
+} = require('../server');
